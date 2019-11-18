d9bbd0fb84738f98e6691c134cad49d8e17514e4
Sydney Sorkin <ssorkin@college.harvard.edu>
2019-11-18 14:56:49 -0500

    twitter auth

--- example.R
+++ example.R
@@ -8,8 +8,13 @@ get_token()
 
 data("babynames")
 
-hockey <- get_timeline("NCAAIceHockey", n = 100) 
-  
+hockey <- get_timeline("NCAAIceHockey", n = 100) %>% 
+  mutate(stripped_text = gsub("http.*","", text)) %>% 
+  mutate(stripped_text = gsub("https.*", "", stripped_text)) %>% 
+  select(status_id, created_at, stripped_text) %>% 
+  unnest_tokens(word, stripped_text) 
+
+
 #gsub says pattern, replacement, x 
 #use mutate
 hockey$stripped_text <- gsub("http.*","",  hockey$text)
@@ -18,7 +23,8 @@ hockey$stripped_text <- gsub("https.*","", hockey$stripped_text)
 hockey_cleaned <- hockey %>% 
   select(status_id, created_at, stripped_text) %>% 
   unnest_tokens(word, stripped_text)
-  #group_by(status_id) use this as id for each distinct tweet
+  
+#group_by(status_id) use this as id for each distinct tweet
 
 #remove stop words
 data("stop_words")
@@ -27,10 +33,9 @@ cleaned_hockey <- hockey_cleaned %>%
   anti_join(stop_words)
 
 #change case of names in babynames data set
-test <- babynames %>%
-  distinct(name, .keep_all = TRUE)
-
-test$name <- str_to_lower(test$name)
+test <- babynames %>% 
+  distinct(name, .keep_all = TRUE) %>% 
+  mutate(name = str_to_lower(name))
 
 #join baby names and words
 
@@ -38,11 +43,35 @@ joined_babynames_tweets <- cleaned_hockey %>%
   left_join(test, by = c("word" = "name")) %>% 
   drop_na() %>% 
   group_by(status_id) %>% 
-  summarise(total_male = sum(sex == "M"))
+  count(sex, sort = TRUE) %>% 
+  ungroup(status_id) %>% 
+  mutate(status_id = reorder(status_id, n))
+
+joined_babynames_tweets %>% 
+  ggplot(aes(x=status_id, y = n)) +
+  geom_col() +
+  facet_wrap(~sex) +
+  coord_flip() +
+  theme(axis.text.y=element_blank())
 
 
 
+#graph 
 
+
+
+joined_babynames_tweets %>%
+  count(word, sort = TRUE) %>%
+  top_n(3) %>% 
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(x = word, y = n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  labs(y = "Count",
+       x = "Unique words",
+       title = "Count of unique words found in tweets",
+       subtitle = "Stop words removed from the list")
 
 #grepl returns logical vector of pattern 
 grepl("eric", hockey_cleaned$word)
---
+++ shiny_files/draft_graph.R
@@ -0,0 +1,51 @@
+
+library(babynames)
+library(rtweet)
+library(tidytext)
+library(tidyverse)
+library(stringr)
+
+get_token()
+
+hockey <- get_timeline("NCAAIceHockey", n = 100) %>% 
+  mutate(stripped_text = gsub("http.*","", text)) %>% 
+  mutate(stripped_text = gsub("https.*", "", stripped_text)) %>% 
+  select(status_id, created_at, stripped_text) %>% 
+  unnest_tokens(word, stripped_text)
+
+#remove stop words
+data("stop_words")
+
+cleaned_hockey <- hockey %>% 
+  anti_join(stop_words)
+
+#babynames dataset
+data("babynames")
+
+#change case of names in babynames data set
+test <- babynames %>% 
+  distinct(name, .keep_all = TRUE) %>% 
+  mutate(name = str_to_lower(name))
+
+#joining names and tweets
+joined_babynames_tweets <- cleaned_hockey %>% 
+  left_join(test, by = c("word" = "name")) %>% 
+  drop_na() %>% 
+  group_by(status_id) %>% 
+  count(sex, sort = TRUE) %>% 
+  ungroup(status_id) %>% 
+  mutate(status_id = reorder(status_id, n))
+
+#graph 
+
+joined_babynames_tweets %>% 
+  ggplot(aes(x=status_id, y = n)) +
+  geom_col() +
+  facet_wrap(~sex) +
+  coord_flip() +
+  theme(axis.text.y=element_blank()) +
+  labs(title = "Gender Mentions in Last 100 Tweets of @NCAAIceHockey",
+       subtitle = "Male names are favored",
+       caption = "Gender assumed by names mentioned in individual Tweet",
+       y = "Count of names mentioned per Tweet",
+       x = "Distinct Tweets")
