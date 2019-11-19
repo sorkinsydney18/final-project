4f9f883a746d3891fd853e0db12e4aafdc4e71fb
Sydney Sorkin <ssorkin@college.harvard.edu>
2019-11-19 11:41:58 -0500

    submitted draft shiny

--- shiny_files/shiny_app_draft.R
+++ shiny_files/shiny_app_draft.R
@@ -6,7 +6,6 @@
 #
 #    http://shiny.rstudio.com/
 #
-source("draft_graph.R")
 library(babynames)
 library(rtweet)
 library(tidytext)
@@ -19,36 +18,9 @@ library(shinythemes)
 data("babynames")
 data("stop_words")
 
-#figure out how to copy token to shiny
-
-consumer_key <- "1G8amEhpNj0qkbU34l9oa1FFa"
-consumer_secret <- "y14jSk4QQFZOXLcU4ogH3w0bYfRtTlNJlguUKDYfYRH36YvAAW"
-app_name <- "gov_1005_proj"
-
-token <- create_token(app_name, consumer_key, consumer_secret, set_renv = TRUE)
-
 
 #create variables for ggplot
-hockey <- get_timeline("NCAAIceHockey", n = 100) %>% 
-  mutate(stripped_text = gsub("http.*","", text)) %>% 
-  mutate(stripped_text = gsub("https.*", "", stripped_text)) %>% 
-  select(status_id, created_at, stripped_text) %>% 
-  unnest_tokens(word, stripped_text)
-
-cleaned_hockey <- hockey %>% 
-  anti_join(stop_words)
-
-test <- babynames %>% 
-  distinct(name, .keep_all = TRUE) %>% 
-  mutate(name = str_to_lower(name))
-
-joined_babynames_tweets <- cleaned_hockey %>% 
-  left_join(test, by = c("word" = "name")) %>% 
-  drop_na() %>% 
-  group_by(status_id) %>% 
-  count(sex, sort = TRUE) %>% 
-  ungroup(status_id) %>% 
-  mutate(status_id = reorder(status_id, n))
+joined_babynames_tweets <- read_rds("joined_babynames_hckytweets.rds")
 
 
 ui <- navbarPage("Project",
